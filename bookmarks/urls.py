80939ac563f207e13b02dfa6c939da00db51c76a
Deepankumar <deepankumar@testpress.in>
2024-09-09 09:58:08 +0000

    Create Image model and fields required to store images and form an views to handle image

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('account/', include('account.urls')),
     path('admin/', admin.site.urls),
     path('social-auth/', include('social_django.urls', namespace='social')),
+    path('images/', include('images.urls', namespace='images')), 
 ]
 
 if settings.DEBUG:
