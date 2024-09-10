5a09d10b1646aaa7ee0ff78a25b7eb3cde3be847
Deepankumar <deepankumar@testpress.in>
2024-09-10 13:24:02 +0000

    Build Ajax View to follow and unfollow Profiles

--- images/views.py
+++ images/views.py
@@ -26,7 +26,7 @@ def image_create(request):
 
 from django.shortcuts import get_object_or_404
 from .models import Image
-
+@login_required
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
     return render(request, 'images/image/detail.html', {'section': 'images', 'image': image})
