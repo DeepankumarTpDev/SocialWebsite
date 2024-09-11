52d5b73df09a10d3ca5fdb33605457bc0292ebf1
Deepankumar <deepankumar@testpress.in>
2024-09-11 06:51:38 +0000

    Add Activity functionality to see the Followers recent Activity

--- images/views.py
+++ images/views.py
@@ -15,6 +15,8 @@ def image_create(request):
             # assign current user to the item
             new_item.user = request.user
             new_item.save()
+            #To create the action to display user activities
+            create_action(request.user, 'bookmarked image', new_item)
             messages.success(request, 'Image added successfully')
             # redirect to new created item detail view
             return redirect(new_item.get_absolute_url())
@@ -34,7 +36,7 @@ def image_detail(request, id, slug):
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from common.decorators import ajax_required
-
+from actions.utils import create_action
 @ajax_required
 @login_required
 @require_POST
@@ -46,6 +48,7 @@ def image_like(request):
             image = Image.objects.get(id=image_id)
             if action == 'like':
                 image.users_like.add(request.user)
+                create_action(request.user, 'likes', image)
             else:
                 image.users_like.remove(request.user)
 
